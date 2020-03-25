10acdcdb4173853cfabae9232cd88ff6757cabb6
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-25 12:02:10 -0600

    Validaciones de formulario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
+  
+  usuario: UsuarioModel;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+  this.usuario = new UsuarioModel();
+  }
 
-
+  onSubmit(form: NgForm) {
+    if (form.invalid){
+      return;
+    }
+    console.log('formulario enviado', this.usuario);
+    console.log(form);
+  }
 }
