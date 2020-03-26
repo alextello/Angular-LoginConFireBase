d9530e66b1d9e0fee55de24ef8f1a367816a0d78
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-26 11:46:29 -0600

    Validación de expiración de token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
   }
   
   logout() {
-
+    localStorage.removeItem('token');
   }
 
   login(usuario: UsuarioModel) {
@@ -61,6 +61,9 @@ export class AuthService {
   private guardarToken(idToken: string) {
     this.userToken = idToken;
     localStorage.setItem('token', idToken);
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+    localStorage.setItem('expira', hoy.getTime().toString());
   }
 
   leerToken() {
@@ -74,7 +77,19 @@ export class AuthService {
   }
 
   estaAutenticado(): boolean {
-    return this.userToken.length > 2;
+    if (this.userToken.length < 2) {
+      return false;
+    }
+
+    const expira = Number(localStorage.getItem('expiera'));
+    const expiraDate  = new Date();
+    expiraDate.setTime(expira);
+
+    if (expiraDate > new Date()){
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
